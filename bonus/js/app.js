af43384f6277dab8dbfebbeeaf3bb9f901a671f0
Filippo Mazzamuto <filippomazzamuto7@gmail.com>
2021-05-28 17:58:29 +0200

    add algoritmo per ricavare i numeri multipli

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,1 +1,12 @@
+//variabili per scegliere il multiplo e da dove iniziare e finire
+var multiple = 3;
+var start = 10;
+var end = 30;
+
 //cicliamo tutti i multipli di 3 compresi tra 10 e 30
+for(var i = start; i <= end; i++){
+    if(i % multiple === 0){
+        console.log(i);
+        document.getElementById('multiples').innerHTML += '<h4>' + i + '</h4>';
+    }
+}
